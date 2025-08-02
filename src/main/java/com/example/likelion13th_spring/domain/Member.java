a84183eb60442ab769e0b0d61ea1f4e73fd23270
jjjaehong <woghd3014@gmail.com>
2025-08-03 01:39:06 +0900

    18주차 세션 과제 커밋

--- src/main/java/com/example/likelion13th_spring/domain/Member.java
+++ src/main/java/com/example/likelion13th_spring/domain/Member.java
@@ -2,6 +2,7 @@ package com.example.likelion13th_spring.domain;
 
 import com.example.likelion13th_spring.enums.Role;
 import jakarta.persistence.*;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
@@ -20,6 +21,7 @@ public class Member {
     private String address;
     private String email;
     private String phoneNumber;
+    private Integer age;
 
     @Enumerated(EnumType.STRING)
     private Role role; // 판매자면 SELLER, 구매자면 BUYER
@@ -41,5 +43,16 @@ public class Member {
         this.deposit -= money;
     }
 
-
+    @Builder
+    public Member(String name, String address, String email, String phoneNumber,
+                  Role role, Boolean isAdmin, Integer deposit, Integer age) {
+        this.name = name;
+        this.address = address;
+        this.email = email;
+        this.phoneNumber = phoneNumber;
+        this.role = role;
+        this.isAdmin = isAdmin;
+        this.deposit = deposit;
+        this.age = age;
+    }
 }
