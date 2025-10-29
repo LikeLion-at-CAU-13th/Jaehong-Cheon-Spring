3c3435e4cc95e3088fe93e02f3f010a1d24ba9fe
jjjaehong <woghd3014@gmail.com>
2025-10-29 15:23:03 +0900

    실습코드

--- src/main/java/com/example/likelion13th_spring/config/SecurityConfig.java
+++ src/main/java/com/example/likelion13th_spring/config/SecurityConfig.java
@@ -2,6 +2,7 @@ package com.example.likelion13th_spring.config;
 
 import com.example.likelion13th_spring.domain.service.CustomOAuth2UserService;
 import com.example.likelion13th_spring.domain.service.CustomUserDetailsService;
+import com.example.likelion13th_spring.jwt.JwtAuthenticationFilter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -9,6 +10,7 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.annotation.web.configurers.CorsConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.web.cors.CorsConfiguration;
@@ -22,32 +24,29 @@ import java.util.Collections;
 public class SecurityConfig {
     private final CustomUserDetailsService customUserDetailsService;
     private final CustomOAuth2UserService customOAuth2UserService;
+    private final JwtAuthenticationFilter jwtFilter;
 
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         // 설정 추가
         http
-                .cors((SecurityConfig::corsAllow)) // CORS 설정
-                .csrf(AbstractHttpConfigurer::disable) // 비활성화
+                .cors((SecurityConfig::corsAllow))
+                .csrf(AbstractHttpConfigurer::disable) // 일반은 비활성화
+                .sessionManagement((manager) -> manager.sessionCreationPolicy(SessionCreationPolicy.STATELESS)) //세션 로그인 안함
+                .httpBasic(AbstractHttpConfigurer::disable) // http basic auth 기반 로그인 인증창 뜨지 않게
+                .formLogin(AbstractHttpConfigurer::disable) // 기본 로그인 페이지 없애기
                 .authorizeHttpRequests((auth) -> auth
-
-                    .requestMatchers("/join", "/login",
-                            "/oauth2/**", "/login/oauth2/**",
-                            "/h2-console/**", "/error").permitAll()
-                    .anyRequest().authenticated())
-                    .oauth2Login(oauth -> oauth
-                            .userInfoEndpoint(userInfo -> userInfo
-                                    .userService(customOAuth2UserService)
-                            )
-                    )
-                        //.requestMatchers("/join", "/login").permitAll() // 회원가입, 로그인은 모두 허용
-                        //.requestMatchers("/**").authenticated()) // 나머지는 인증된 사용자만 허용
-//                .formLogin(Customizer.withDefault())
-//                .logout(Customizer.withDefault())
-                .userDetailsService(customUserDetailsService)
-        ;
-
+                        .requestMatchers("/join", "/login").permitAll() // 모두 허용
+                        .requestMatchers("/**").authenticated()) // 인증된 사용자만 허용
+//                .oauth2Login(oauth -> oauth
+//                        .userInfoEndpoint(userInfo -> userInfo
+//                                .userService(customOAuth2UserService)
+//                        )
+//                )
+//            .formLogin(Customizer.withDefaults()) // login 설정
+//            .logout(Customizer.withDefaults()) // logout 설정
+                .userDetailsService(customUserDetailsService);
         return http.build();
     }
     @Bean
--- src/main/java/com/example/likelion13th_spring/controller/JoinController.java
+++ src/main/java/com/example/likelion13th_spring/controller/JoinController.java
@@ -1,7 +1,10 @@
 package com.example.likelion13th_spring.controller;
 
+import com.example.likelion13th_spring.domain.Member;
 import com.example.likelion13th_spring.domain.service.MemberService;
 import com.example.likelion13th_spring.dto.request.JoinRequestDto;
+import com.example.likelion13th_spring.dto.response.TokenResponseDto;
+import com.example.likelion13th_spring.jwt.JwtTokenProvider;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -12,11 +15,17 @@ import org.springframework.web.bind.annotation.RestController;
 public class JoinController {
 
     private final MemberService memberService;
+    private final JwtTokenProvider jwtTokenProvider;
 
     @PostMapping("/join")
     public void join(@RequestBody JoinRequestDto joinRequestDto) {
         memberService.join(joinRequestDto);
     }
 
+    @PostMapping("/login")
+    public TokenResponseDto login(@RequestBody JoinRequestDto joinRequestDto) {
+        Member member = memberService.login(joinRequestDto);
+        return TokenResponseDto.of(jwtTokenProvider.generateAccessToken(member.getName()), jwtTokenProvider.generateRefreshToken(member.getName()));
+    }
 }
 
